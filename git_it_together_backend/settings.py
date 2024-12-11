da4f8fe23247b434ee8bea125751d447bf8ef87b
BuickSalesman <adam.odonnell.e@gmail.com>
2024-12-11 17:14:06 -0500

    fixed typo in settings.py

--- git_it_together_backend/settings.py
+++ git_it_together_backend/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'git_it_together_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        DB_NAME       'DIRS': [],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'git_it_together_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config(''),
+        'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
