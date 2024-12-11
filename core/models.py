3859430f5a5f9f3c307b62c4eed137c46aa3465a
BuickSalesman <adam.odonnell.e@gmail.com>
2024-12-11 18:30:51 -0500

    added model template to models.py and core to INSTALLED_APPS

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+  # some stuff
+
+
+class Repo(models.Model):
+  # some stuff
+
+
+class Commit(models.Model):
+  # some stuff
