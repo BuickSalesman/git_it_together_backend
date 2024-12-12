5dbfb4d0492b007fc8cb5e4ff18022a9796ad8c8
BuickSalesman <adam.odonnell.e@gmail.com>
2024-12-12 16:12:39 -0500

    added database models

--- core/models.py
+++ core/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-
-# Create your models here.
-
-
-class User(models.Model):
-  # some stuff
+from django.contrib.auth.models import User
 
 
 class Repo(models.Model):
-  # some stuff
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    notes_enabled = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Commit(models.Model):
-  # some stuff
+    repo = models.ForeignKey(Repo, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    note_title = models.CharField(max_length=100, null=True, blank=True)
+    note_body = models.TextField(null=True, blank=True)
