49af8bb80c24033350a0743b85d1c19af0e9d785
BuickSalesman <adam.odonnell.e@gmail.com>
2024-12-17 21:02:17 -0500

    made a view function

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
+from django.http import JsonResponse
+
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import User
+
+
+def get_users(req):
+    users = User.objects.all()
+    users_data = [
+        {
+            "id": user.id, "username": user.username,
+        }
+        for user in users
+    ]
+    return JsonResponse({"users": users_data})
--- git_it_together_backend/settings.py
+++ git_it_together_backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'git_it_together_backend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
