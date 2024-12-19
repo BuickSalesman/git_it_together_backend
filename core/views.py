f3c80de52f0ee8bb219c1cae54eb21b77cdf98ca
BuickSalesman <adam.odonnell.e@gmail.com>
2024-12-18 22:22:27 -0500

    started work on creating repo post method

--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from .models import User
+from .models import Repo
 
 
 def get_users(req):
@@ -16,3 +17,7 @@ def get_users(req):
         for user in users
     ]
     return JsonResponse({"users": users_data})
+
+
+def create_repo(req):
+    repo = Repo
