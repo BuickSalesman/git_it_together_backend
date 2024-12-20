a4d313eec7382f2dbe58fee3e88f4a9dd77eceda
BuickSalesman <adam.odonnell.e@gmail.com>
2024-12-20 12:11:45 -0500

    added csrf exempt to create user

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 import json
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from django.shortcuts import render
 
@@ -21,6 +22,7 @@ def get_users(request):
     return JsonResponse({"users": users_data})
 
 
+@csrf_exempt
 def create_user(request):
     if request.method == 'POST':
         try:
