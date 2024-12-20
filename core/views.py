c7dba2aabc741640e184f155e06705cf766008d8
BuickSalesman <adam.odonnell.e@gmail.com>
2024-12-20 12:06:05 -0500

    added create user view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+import json
 from django.http import JsonResponse
 
 from django.shortcuts import render
@@ -8,7 +9,8 @@ from .models import User
 from .models import Repo
 
 
-def get_users(req):
+# retrieves users via get request to the database.
+def get_users(request):
     users = User.objects.all()
     users_data = [
         {
@@ -19,5 +21,27 @@ def get_users(req):
     return JsonResponse({"users": users_data})
 
 
-def create_repo(req):
-    repo = Repo
+def create_user(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            username = data.get('username')
+            password = data.get('password')
+
+            if not username or not password:
+                return JsonResponse({"error": "Username and password are required."}, status=400)
+
+            if User.objects.filter(username=username).exists():
+                return JsonResponse({"error": "A user with that username already exists"}, status=400)
+
+            user = User.objects.create_user(
+                username=username, password=password)
+
+            return JsonResponse({"message": "User created successfully", "user_id": user.id}, status=200)
+
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON data."}, status=400
+                                )
+
+    else:
+        return JsonResponse({"error": "This endpoint only supports POST requests."}, status=405)
