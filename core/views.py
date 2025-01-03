d091ed97fecb4e804ef50db6f64140f9329d1471
BuickSalesman <adam.odonnell.e@gmail.com>
2025-01-02 20:25:51 -0500

    refactored repo deletion into DRF conventions

--- core/views.py
+++ core/views.py
@@ -153,39 +153,28 @@ def create_repo(request):
     }, status=201)
 
 
-@csrf_exempt
+@api_view(["DELETE"])
+@permission_classes({IsAuthenticated})
 def delete_repo(request):
-    if request.method == "DELETE":
-        try:
-            data = json.loads(request.body)
-            username = data.get("username")
-            name = data.get("name")
+    user = request.user
+    data = request.data
+    repo_name = data.get("name")
 
-            if not username or not name:
-                return JsonResponse({"error": "Username and repo name are required"}, status=400)
+    if not repo_name:
+        return Response({"error": "Repository name is required"}, status=400)
 
-            try:
-                user = User.objects.get(username=username)
-            except User.DoesNotExist:
-                return JsonResponse({"error": "User does not exist"}, status=404)
+    try:
+        repo = Repo.objects.get(user=user, name=repo_name)
+    except Repo.DoesNotExist:
+        return Response({
+            "error": "Repository not found or doesn't belong to current user."
+        }, status=404)
 
-            try:
-                repo = Repo.objects.get(user=user, name=name)
-            except Repo.DoesNotExist:
-                return JsonResponse({"error": "Repository not found for this user"}, status=404)
+    repo.delete()
+    return Response({"message": "Repo deleted successfully"}, status=200)
 
-            repo.delete()
-
-            return JsonResponse({"message": "Repository deleted successfully"}, status=200)
-
-        except json.JSONDecodeError:
-            return JsonResponse({"error": "Invalid JSON data"}, status=400)
-
-    else:
-        return JsonResponse({"error": "This endpoint only supports DELETE requests"}, status=405)
 
 # endregion REPOS
-
 # region COMMITS
 
 
